ffd3af0ccfa9037829f81baaabf1a7eae3306ca1
DevilScript <52322852+DevilScript@users.noreply.github.com>
2025-04-17 21:25:23 +0700

    Update 20250408000000_create_profiles_table.sql

--- supabase/migrations/20250408000000_create_profiles_table.sql
+++ supabase/migrations/20250408000000_create_profiles_table.sql
@@ -1,13 +1,13 @@
 create or replace function update_balance_and_log_transaction(p_user_id text, p_amount numeric, p_voucher_hash text)
- returns void as $$
- begin
-   -- อัพเดท balance
-   update profiles
-   set balance = balance + p_amount
-   where user_id = p_user_id;
- 
-   -- บันทึก transaction
-   insert into truemoney_transactions (user_id, amount, voucher_hash, status)
-   values (p_user_id, p_amount, p_voucher_hash, 'success');
- end;
- $$ language plpgsql;
+returns void as $$
+begin
+  -- อัพเดท balance
+  update profiles
+  set balance = balance + p_amount
+  where user_id = p_user_id;
+
+  -- บันทึก transaction
+  insert into truemoney_transactions (user_id, amount, voucher_hash, status)
+  values (p_user_id, p_amount, p_voucher_hash, 'success');
+end;
+$$ language plpgsql;
